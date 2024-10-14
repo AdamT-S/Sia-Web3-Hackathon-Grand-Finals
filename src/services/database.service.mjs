853d686d05e236330f135fe0dac86776eab54e05
AdamT-S <158591813+AdamT-S@users.noreply.github.com>
2024-10-14 20:27:34 +0100

    Added more SQL commands (add_user, add_Admin, add_tokens, rm_tokens)

--- src/services/database.service.mjs
+++ src/services/database.service.mjs
@@ -19,7 +19,7 @@ export default class DatabaseService {
 		return new DatabaseService(conn);
 	}
 
-	/* Get a single continent */
+	/* User Login */
 	async Login(email, password) {
 		try {
 			const sql = `
@@ -38,4 +38,58 @@ export default class DatabaseService {
 		}
 	}
 
+	async Add_tokens(email, more_tokens){
+		try{
+			const sql = `
+			UPDATE userinfo 
+			SET tokens = tokens + ${more_tokens}
+			WHERE email = "${email}"; 
+			`
+		}
+		catch (err) {
+			console.error('cannot add tokens:', err);
+			return [];
+	}
 }
+	async rm_tokens(email, less_tokens){
+		try{
+			const sql = `
+			UPDATE userinfo 
+			SET tokens = tokens - ${less_tokens}
+			WHERE email = "${email}"; 
+			`
+		}
+		catch (err) {
+			console.error('cannot remove tokens:', err);
+			return [];
+		}
+	}
+
+	async add_User(email, password){
+		try{
+			const sql = `
+			INSERT INTO userinfo (email, user_pass, tokens, user_type)
+			VALUES ("${email}", "${password}", 0, "endUser")
+			`
+		}
+		catch (err) {
+			console.error('cannot add user:', err);
+			return [];
+		}
+	}
+
+	async add_Admin(email, password){
+		try{
+			const sql = `
+			INSERT INTO userinfo (email, user_pass, tokens, user_type)
+			VALUES ("${email}", "${password}", 0, "Admin")
+			`
+		}
+		catch (err) {
+			console.error('cannot add Admin:', err);
+			return [];
+		}
+	}
+
+}
+
